a18b314963405ba8a9aa4633bd62a9fdfc74b5df
Hashim Hossam Mahmoud Mohammed <cse-hashim@users.noreply.github.com>
2021-12-21 18:27:16 +0200

    add entry

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path")
+const webpack = require("webpack")
+module.exports = {
+    entry: './src/client/index.js',//webpack entry
+    
+    
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.js$/,
+    //       exclude: /node_modules/,
+    //       use: {
+    //         loader: "babel-loader"
+    //       }
+    //     }
+    //   ]
+    // }
+  
+
+}
